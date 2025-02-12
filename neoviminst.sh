72af4ead7af749ae8f91c7fb23fe0665cbf81d83
jamesonhm <jamesonhmarshall@gmail.com>
2025-02-12 08:23:36 -0700

    update neovim inst to use /usr/local

--- neoviminst.sh
+++ neoviminst.sh
@@ -4,11 +4,12 @@ sudo apt-get install -y \
 	gettext libtool libtool-bin \
 	autoconf automake cmake g++ \
 	pkg-config unzip
+cd ~/
 git clone https://github.com/neovim/neovim.git
 
 cd neovim
 git checkout stable
-make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/usr/local install
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
 sudo apt install -y npm xclip
